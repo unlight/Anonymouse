e25d18e65b4d0734b6d857a5f5d0d69a046ac576
Ronnie <unlights@gmail.com>
2010-12-14 17:15:32 +0300

    Removed debug code

--- anonymouse.js
+++ anonymouse.js
@@ -2,7 +2,6 @@ jQuery(document).ready(function(){
 	
 	$('#Error_Form_Captcha').livequery(function(){
 		var newSrc = gdn.url('plugins/Anonymouse/captcha/imagettfbox.php') + '?' + Math.random();
-		console.log(newSrc);
 		$('#CaptchaBox img').first().attr('src', newSrc);
 
 	});
